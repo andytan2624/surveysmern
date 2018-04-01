a92daac9aed1d89b89f58680f9b1c8eefc07a9be
andytan2624 <andy.tan2624@gmail.com>
2018-04-02 07:49:22 +1000

    Finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+// dev.js - dont commit this
+
+module.exports = {
+    googleClientID: '759931439146-vjrh7aosvo6e0su9f6qn2jfif7anrmeg.apps.googleusercontent.com',
+    googleClientSecret: 'gt9haW3Gu-U-nS97ntKibDJS',
+    mongoURI: 'mongodb://ash_skywalker10:password@ds155268.mlab.com:55268/emaily-ashskywalker10',
+    cookieKey: 'fipuhdhhviehirgh8932yrfheskfksdefs'
+};
